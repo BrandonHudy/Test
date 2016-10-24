027dd02866fab897aacf4545525eaccc9828dc43
brandonhudy <hudy.brandon@gmail.com>
2016-10-23 23:13:10 -0600

    Wrote ListElement class

--- ListElement.java
+++ ListElement.java
@@ -1,5 +1,20 @@
 package lab3;
 
 public class ListElement {
-
+	
+	private ListElement next;
+	private int data;
+	
+	public ListElement(){
+		this.data = 0;
+		this.next = null;
+	}
+	
+	public void setData(int data){
+		this.data = data;
+	}
+	
+	public int getData(){
+		return this.data;
+	}
 }
